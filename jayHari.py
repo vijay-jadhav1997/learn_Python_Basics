e8425c9a2f265eca5fdb578cb36344894e307aec
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-01-23 19:43:20 +0530

    learn print fn & some string methods like strip, title, captalize, etc.

--- jayHari.py
+++ jayHari.py
@@ -4,16 +4,22 @@
 # Ask user for their name
 name = input("What's your name? ")
 
-# Remove all unnecessary whatspaces from string using '.strip()' string method
-name = name.strip()
+#* Remove all unnecessary whatspaces from string using '.strip()' string method
+name = name.strip().title()
+userName = name.title().strip()
 
+#* Capitalize each word of string using '.title()' method: Return a version of the string where each word is titlecased.
+# name = name.title()
+
+#? method chaining ".strip().title()" :
 
 # Say Jay Hari to user
 #* 1) 1st method:
-print("Jay Hari, " + name)
+# print("Jay Hari, " + name)
 
 #* 2) 2st method:
-print("Jay Hari, ", name)
+# print("Jay Hari, ", name)
 
-#* 3) 3rd method:
-print(f"Jay Hari, {name}") #
+#* 3) 3rd method: using f"${varName}"
+print(f"Jay Hari, {name}" + " Govind") 
+print(f"Jay Hari, {userName}" ) 

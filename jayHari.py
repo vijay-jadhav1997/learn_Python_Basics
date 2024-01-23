ed48d317ca2e9258fdc67cc029a5fb79d6616e8a
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-01-23 19:06:35 +0530

    start to learn Python basics

---
+++ jayHari.py
@@ -0,0 +1,19 @@
+# print 'jay hari'  
+# print("Jay Hari to All of devotees of Hari & Har ....!")
+
+# Ask user for their name
+name = input("What's your name? ")
+
+# Remove all unnecessary whatspaces from string using '.strip()' string method
+name = name.strip()
+
+
+# Say Jay Hari to user
+#* 1) 1st method:
+print("Jay Hari, " + name)
+
+#* 2) 2st method:
+print("Jay Hari, ", name)
+
+#* 3) 3rd method:
+print(f"Jay Hari, {name}") #

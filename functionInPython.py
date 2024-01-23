6df499d92558d8674fd738579f1924dbfd669ca6
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-01-23 20:37:00 +0530

    start learning declaration of function in Python

---
+++ functionInPython.py
@@ -0,0 +1,13 @@
+def square(num):
+    return num * num
+
+numSqr = square(5)
+
+print(numSqr)
+
+
+def welcomeMsg():
+    return "**************\n*           *\n*   WELCOME!    *\n*           *\n**************"
+
+
+print(welcomeMsg)

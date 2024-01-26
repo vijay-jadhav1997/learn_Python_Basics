741b6581fb48aaa469d62396d81ff79a266d5606
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-01-26 18:55:51 +0530

    function declaration or defination in python

--- functionInPython.py
+++ functionInPython.py
@@ -3,11 +3,33 @@ def square(num):
 
 numSqr = square(5)
 
-print(numSqr)
+# Sprint(numSqr)
 
 
 def welcomeMsg():
     return "**************\n*           *\n*   WELCOME!    *\n*           *\n**************"
 
 
-print(welcomeMsg)
+# print(welcomeMsg)
+
+def greeting(userName="user"):
+    print(f"Welcome dear, {userName.title()} !")
+
+# userInp = input("Enter your name here => ")
+
+
+# greeting(userInp)
+# greeting()
+
+
+def main():
+    userName = input("what's your name? => ").title()
+    hello(userName)
+    hello()
+
+
+def hello(user_name="user"):
+    print(f"Jay Hari, dear {user_name}!")
+
+
+main()

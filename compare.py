24a671bc56fe0460900f5930400e22a8a17ebd1e
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-01-27 21:11:48 +0530

    update: compare.py

--- compare.py
+++ compare.py
@@ -2,6 +2,13 @@ name = (input("what's your name?  => ")).strip().title()
 x = int(input("what is x?  => "))
 y = int(input("what is y?  => "))
 
+#* print output using 'or' & 'and' logical operators
+x > y and  print(f"Hello dear {name} ji, x({x}) is greater than y({y}). ")
+x != y or print(f"Hello dear {name} ji, x({x}) is equal to y({y}). ")
+# x == y and print(f"Hello dear {name}, x({x}) is equal to y({y}). ")
+x < y and print(f"Hello dear {name} ji, y({y}) is greater than x({x}). ")
+
+
 #? define if_else statement :
 # if x > y:
 #   print(f"Hello dear {name}, x({x}) is greater than y({y}). ")
@@ -18,7 +25,12 @@ y = int(input("what is y?  => "))
 #   print(f"Hello dear {name}, x({x}) is equal to y({y}). ")
 
 #? Yet another way
-if x == y:
-  print(f"Hello dear {name}, x({x}) is equal to y({y}). ")
-else:
-  print(f"Hello dear {name}, x({x}) is not equal to y({y}). ")
+# if x == y:
+#   print(f"Hello dear {name}, x({x}) is equal to y({y}). ")
+# else:
+#   print(f"Hello dear {name}, x({x}) is not equal to y({y}). ")
+
+
+#* Yet again another way using 'or' & 'and' logical operators
+# x == y or print(f"Hello dear {name}, x({x}) is not equal to y({y}). ")
+# x == y and print(f"Hello dear {name}, x({x}) is equal to y({y}). ")

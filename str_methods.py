3274e8c022a1383fa253d7dd83950050881fe0ff
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-01-30 20:45:25 +0530

    learn few more str methods

--- str_methods.py
+++ str_methods.py
@@ -1,6 +1,44 @@
-inp = input("enter anything... ")
+date_inp = input("Date: ")
 
-if inp.isalpha():
-  print("Valid: ✅")
-else:
-  print("Invalid: ❌")
+# if inp[0].isdigit():
+#   print(f"Valid: ✅ => {int(inp)}")
+# else:
+#   print("Invalid: ❌")
+
+#! str methods that returns True/False
+date_inp.isalpha()
+date_inp.isdigit()
+date_inp.isspace()
+date_inp.isnumeric()
+date_inp.islower()
+date_inp.isupper()
+date_inp.isalnum()
+date_inp.istitle()
+date_inp.isidentifier()
+
+#! str that returns index number
+date_inp.index()
+date_inp.find("substring")
+
+#! str that returns copy of str
+# date_inp[i : j]
+date_inp.capitalize()
+date_inp.title()
+date_inp.lower()
+date_inp.upper()
+date_inp.endswith("substring")
+date_inp.startswith("substring")
+# date_inp.center(width, _fill_character)
+# date_inp.rjust(width, _fill_character)
+# date_inp.ljust(width, _fill_character)
+date_inp.join(["str1", "str2", "str3"])
+date_inp.format()
+date_inp.encode()
+
+
+if date_inp[0].isdigit():
+  month, day, year = date_inp.split("/")
+  month = int(month)
+  day = int(day)
+  print(day)
+  print(year.isspace())

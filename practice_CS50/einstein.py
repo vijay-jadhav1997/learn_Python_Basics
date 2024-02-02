8f3bdf9dbfe9bf68074e4e6cf67dc31cd764c3c3
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-02-02 20:20:16 +0530

    Create einstein.py

---
+++ practice_CS50/einstein.py
@@ -0,0 +1,9 @@
+# Promt user input and convert str to int
+mass = int(input("m : "))
+
+# Convert mass to equivalent energy
+speed_sqr = 300000000 ** 2
+energy = mass * speed_sqr
+
+# Display energy
+print(f"E : {energy}")

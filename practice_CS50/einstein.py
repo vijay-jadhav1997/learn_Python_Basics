4a1dd1dd1de22fc3360e7c69c6628795a4b4f5d5
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-02-15 19:12:22 +0530

    Docs: changes in file structure

--- practice_CS50/einstein.py
+++
@@ -1,9 +0,0 @@
-# Promt user input and convert str to int
-mass = int(input("m : "))
-
-# Convert mass to equivalent energy
-speed_sqr = 300000000 ** 2
-energy = mass * speed_sqr
-
-# Display energy
-print(f"E : {energy}")

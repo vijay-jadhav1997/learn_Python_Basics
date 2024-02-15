26ac11433f70f9a0c04dd904afd59650b4742e0e
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-02-15 19:11:41 +0530

    learn basic built-in functions of Python

---
+++ Python_Basics/Data_Types/built_in_fn.py
@@ -0,0 +1,17 @@
+line1 = "************@@##@@*************"
+line2 = "**                           **"
+line3 = "**         JAY HARI!         **"
+
+sum = 51
+print("")
+print(line1)
+print(line2)
+print(line3)
+print(line2)
+print(line1)
+print("Jay HAri!" if sum == 5 else "Jay Jay Ram Krushna Hari")
+
+print(type(sum))
+print(type(sum) == int)
+print(isinstance(line1, str))
+print(isinstance(line1, int))

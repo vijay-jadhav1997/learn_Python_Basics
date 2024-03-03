6bd20b0c6ae98cd2ab5029058652b857a6d40bed
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-03-03 19:34:39 +0530

    learn loop statements in Python
    learn 'while', 'while-else', 'for' & 'for-else' loops

---
+++ Python_Basics/loops.py
@@ -0,0 +1,19 @@
+count = 10
+# while count > 5:
+#   print(count)
+#   count -= 1
+
+# while count > 0:
+#   print(f"This is while statement when count = {count}")
+#   count -= 1
+# else:
+#   print(f"This is else statement when count = {count}")
+
+for i in range(1,6):
+  print(i)
+
+
+for i in range(5,11):
+  print(f"Jay Hari {i} => for statement")
+else:
+  print(f"Jay Hari ✨ => else statement")
